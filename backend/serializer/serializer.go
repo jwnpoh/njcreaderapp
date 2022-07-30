73dc872f774ac2c8ee87177bf8172ec9dc4d0b1f
jwnpoh <jwn.poh@gmail.com>
2022-07-31 01:00:23 +0800

    update wip

--- backend/serializer/serializer.go
+++ backend/serializer/serializer.go
@@ -9,7 +9,7 @@ import (
 
 type Serializer interface {
 	Encode(w http.ResponseWriter, status int, headers ...http.Header) error
-	Decode(w http.ResponseWriter, r *http.Request) error
+	// Decode(w http.ResponseWriter, r *http.Request, data any) error
 	ErrorJson(w http.ResponseWriter, err error, status ...int) error
 }
 
@@ -19,7 +19,7 @@ type serializer struct {
 	Data    any    `json:"data,omitempty"`
 }
 
-func NewSerializer(err bool, msg string, data any) Serializer {
+func NewSerializer(err bool, msg string, data ...any) Serializer {
 	return &serializer{
 		Error:   err,
 		Message: msg,
@@ -50,13 +50,13 @@ func (s *serializer) Encode(w http.ResponseWriter, status int, headers ...http.H
 	return nil
 }
 
-func (s *serializer) Decode(w http.ResponseWriter, r *http.Request) error {
+func Decode(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1048576
 
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	dec := json.NewDecoder(r.Body)
-	err := dec.Decode(s.Data)
+	err := dec.Decode(data)
 	if err != nil {
 		return fmt.Errorf("unable to decode json data - %w", err)
 	}
