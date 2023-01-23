5894f4526137b3eb1e92f3dfed3e3ac5e3060800
jwnpoh <jwn.poh@gmail.com>
2023-01-24 00:39:49 +0800

    component for future implementation of notifications function

---
+++ frontend/the-njc-reader/src/lib/ActivityNotificationsButton.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Icon } from "svelte-awesome";
+  import users from "svelte-awesome/icons/users";
+</script>
+
+<button
+  class:active={active === "activity"}
+  on:click={() => (active = "activity")}
+>
+  <div class="indicator">
+    <span
+      class="indicator-item badge badge-xs badge-accent w-fit translate-x-8 md:translate-x-10"
+      >new</span
+    >
+    <Icon data={users} scale={1.6} />
+  </div>
+  <p class="text-xs md:text-sm">Activity</p>
+</button>
