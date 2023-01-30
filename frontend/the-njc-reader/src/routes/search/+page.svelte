5a72b63a10dc3459633c1f110b8af080d1f99b82
jwnpoh <jwn.poh@gmail.com>
2023-01-30 22:50:27 +0800

    fix #19

--- frontend/the-njc-reader/src/routes/search/+page.svelte
+++ frontend/the-njc-reader/src/routes/search/+page.svelte
@@ -1,6 +1,10 @@
 <script>
+  import { page } from "$app/stores";
   import CardContainer from "$lib/CardContainer.svelte";
   import PageTitle from "$lib/PageTitle.svelte";
+
+  let user = $page.data.user;
+
   export let data;
   const error = data.error;
   const message = data.message;
@@ -10,7 +14,7 @@
 
 <PageTitle>Search</PageTitle>
 {#if !error}
-  <CardContainer {articles} {query} />
+  <CardContainer {articles} {query} loggedIn={user.loggedIn}/>
 {:else}
   {message}
 {/if}
