9033ae85524be4d27c52d1de9b65e5e8b58a53e1
jwnpoh <jwn.poh@gmail.com>
2025-03-19 20:35:27 +0800

    update 2024 questions list

--- cockroach-migration-tool/schema-migration/update-new-questions.sql
+++ cockroach-migration-tool/schema-migration/update-new-questions.sql
@@ -4,63 +4,44 @@ INSERT INTO question_list (
 	number, 
 	wording
 ) VALUES (
-	'2023 - Q1',
-	'2023',
+	'2024 - Q1',
+	'2024', 
 	'1',
-	'How realistic is it for countries to implement a national minimum wage for all their workers?'	
-), (
-	-- '2023 - Q2', inserted this question first on its own to test the SQL command 
-	-- '2023', 
-	-- '2', 
-	-- '‘Fossil fuels should no longer have a part in the production of energy.’ Discuss.'
--- ), (
-	'2023 - Q3',
-	'2023',
+	'To what extent are science and technology able to solve the problem of waste disposal?'
+),(
+	'2024 - Q2',
+	'2024', 
+	'2',
+	'How desirable is it for a country to provide free healthcare for all its citizens?'
+),(
+	'2024 - Q3',
+	'2024', 
 	'3',
-	'Consider the view that spending money on space travel cannot be justified in today’s world.'
-), (
-	'2023 - Q4',
-	'2023',
+	'Evaluate the measures taken in your society to deter crime and punish criminals.'
+),(
+	'2024 - Q4',
+	'2024', 
 	'4',
-	'Consider the argument that there should be no censorship of the arts in modern society.'
-), (
-	'2023 - Q5',
-	'2023',
+	'‘Education today should involve more than the study of academic subjects.’ How far do you agree?'
+),(
+	'2024 - Q5',
+	'2024', 
 	'5',
-	'‘People who undertake voluntary work do so more for their own benefit than for the benefit of others.’ Discuss.'
-), (
-	'2023 - Q6',
-	'2023',
+	'Assess the view that it is always right to challenge injustice.'
+),(
+	'2024 - Q6',
+	'2024', 
 	'6',
-	'Assess the extent to which all people in your society have the opportunity to achieve their full potential.'
-), (
-	'2023 - Q7',
-	'2023',
+	'‘Online advertisements use increasingly sophisticated methods to target consumers.’ To what extent does this bring more harm than good?'
+),(
+	'2024 - Q7',
+	'2024', 
 	'7',
-	'‘The quality of human interaction is diminished by modern communication devices.’ How far do you agree?'
-), (
-	'2023 - Q8',
-	'2023',
+	'‘Humour is essential for an individual''s well-being.’ Discuss.'
+),(
+	'2024 - Q8',
+	'2024', 
 	'8',
-	'To what extent are festivals and national holidays effective in promoting unity in your society?'
-), (
-	'2023 - Q9',
-	'2023',
-	'9',
-	'‘Regret for past actions is vital for progress.’ What is your view?'
-), (
-	'2023 - Q10',
-	'2023',
-	'10',
-	'Evaluate the claim that sports personalities make good role models for young people.'
-), (
-	'2023 - Q11',
-	'2023',
-	'11',
-	'Assess the view that accurate translation between languages is always necessary.'
-), (
-	'2023 - Q12',
-	'2023',
-	'12',
-	'‘Young people want to change the world because they do not know it is impossible.’ How far do you agree?'
+	'‘There is a lack of appreciation for the value of music.’ How far is this true in your society?'
 );
+
