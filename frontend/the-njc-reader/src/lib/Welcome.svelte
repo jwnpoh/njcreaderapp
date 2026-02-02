4819ac0053914e458fd67a27111dd4fad3b7f7a4
jwnpoh <jwn.poh@gmail.com>
2026-02-02 23:00:45 +0800

    Modify welcome message to be collapsible.

--- frontend/the-njc-reader/src/lib/Welcome.svelte
+++ frontend/the-njc-reader/src/lib/Welcome.svelte
@@ -1,16 +1,26 @@
-<div class="grid gap-6 my-6 px-10 md:px-28 lg:px-52 text-justify">
-  <h1 class="font-bold text-2xl">Welcome!</h1>
-  <p class="text-md">
-    Too many articles and sources but not enough time to read them all?
-  </p>
-  <p class="text-md">
-    Your teachers have curated articles which are relevant to our course for
-    your daily reading. Each article is appended with relevant past year 'A' Level essay
-    questions for you to practise making connections between the news you read and the issues discussed.
-  </p>
+<div
+  class="collapse collapse-arrow box-shadow bg-base-100 rounded-box my-6 mx-10 md:mx-28 lg:mx-52"
+>
+  <input type="checkbox" checked />
+  <div class="collapse-title text-2xl font-bold">Welcome!</div>
+  <div class="collapse-content text-justify grid gap-6">
+    <p class="text-md">
+      Too many articles and sources but not enough time to read them all?
+    </p>
+    <p class="text-md">
+      Your teachers have curated articles which are relevant to our course for
+      your daily reading. Each article is appended with relevant past year 'A'
+      Level essay questions for you to practise making connections between the
+      news you read and the issues discussed.
+    </p>
 
-  <p class="text-md">Log in to enjoy more features such as the Social Notebook and Longer Reads!</p>
-
-  <p class="text-md">We hope that you find this resource helpful!</p>
+    <p class="text-md">We hope that you find this resource helpful!</p>
+  </div>
 </div>
 <div class="divider" />
+
+<style>
+  .box-shadow {
+    box-shadow: 1px 0 8px 0 rgb(0 0 0 / 0.1);
+  }
+</style>
